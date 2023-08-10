a5a9113696950fc1bb662f42ac64781af63b96c1
Matheus Cruz <127210839+Matheus-S-Cruz@users.noreply.github.com>
2023-08-10 12:55:42 -0300

    Projeto Logica

--- script.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.getElementById('text');
-const copyButton = document.getElementById('copy');
-
-copyButton.addEventListener('click', ()=> {
-  textInput.select();
-  document.execCommand('copy');
-});
